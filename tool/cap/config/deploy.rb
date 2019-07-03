41b183f4215b6707b2f9be2211825077ef2a342d
Rizwan Khan <rizwanwakil2018@gmail.com>
2019-07-03 20:58:10 +0400

    add all change regarding capistrano installation

---
+++ tool/cap/config/deploy.rb
@@ -0,0 +1,43 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.11.0"
+
+set :application, "magento_store"
+set :repo_url, "git@github.com:rizwanwakil2018/magento2store.git"
+set :magento_deploy_languages, ['en_US', 'en_GB']
+set :magento_auth_public_key, '6d304bfc5ac8e8cdac4c60c14247ca39'
+set :magento_auth_private_key, '65efe00a6ff3fcaf29d8ac13aef70531'
+set :keep_releases, 3
+
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+# set :deploy_to, "/var/www/my_app_name"
+
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# append :linked_files, "config/database.yml"
+
+# Default value for linked_dirs is []
+# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for local_user is ENV['USER']
+# set :local_user, -> { `git config user.name`.chomp }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+# Uncomment the following to require manually verifying the host key before first deploy.
+# set :ssh_options, verify_host_key: :secure
