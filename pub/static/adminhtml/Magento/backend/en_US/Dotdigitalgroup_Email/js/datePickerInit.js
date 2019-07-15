50809594824f16cf0220238f2f50056e8067784c
Rizwan Khan <rizwanwakil2018@gmail.com>
2019-07-15 23:11:34 +0400

    working magento

--- pub/static/adminhtml/Magento/backend/en_US/Dotdigitalgroup_Email/js/datePickerInit.js
+++ pub/static/adminhtml/Magento/backend/en_US/Dotdigitalgroup_Email/js/datePickerInit.js
@@ -1,10 +1,1 @@
-require(['jquery', 'jquery/ui', 'domReady!'], function ($) {
-    'use strict';
-
-    var el = $('.ddg-datepicker');
-
-    el.datepicker({
-        dateFormat: 'yy-mm-dd'
-    });
-    el.addClass('datepicker');
-});
+/var/www/html/dev_env/vendor/dotmailer/dotmailer-magento2-extension/view/adminhtml/web/js/datePickerInit.js
